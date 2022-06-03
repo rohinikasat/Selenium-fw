376feec525fc5f6cf0d94ec8725a32683d899f19
Rohini Kasat <rohinikasat@gmail.com>
2022-06-03 02:25:28 -0700

    add comment to base test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+//this is base test
 public class BaseTest {
 
 	public static WebDriver driver;
